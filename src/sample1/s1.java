fda3b378e538fd4cf224a4b424dc2945667bea43
Chetana <Chetana@MyPc>
2022-07-17 16:45:07 +0530

    [chetana:] added new file

---
+++ src/sample1/s1.java
@@ -0,0 +1,14 @@
+package sample1;
+
+public class s1 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		
+		
+	}
+
+}
